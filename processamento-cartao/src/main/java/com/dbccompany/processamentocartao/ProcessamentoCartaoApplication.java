445147539674178964a843e9e11e78a871a33b9c
Ezequias Ferreira <ezequiasbarros1997@gmail.com>
2022-04-15 14:56:38 -0300

    Adição da api de processamento

---
+++ processamento-cartao/src/main/java/com/dbccompany/processamentocartao/ProcessamentoCartaoApplication.java
@@ -0,0 +1,13 @@
+package com.dbccompany.processamentocartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessamentoCartaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessamentoCartaoApplication.class, args);
+    }
+
+}
