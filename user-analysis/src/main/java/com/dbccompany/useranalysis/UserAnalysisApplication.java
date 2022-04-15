a9184476dabdb5e3917f7643b96a8bfe5c6ccbb2
Ezequias Ferreira <ezequiasbarros1997@gmail.com>
2022-04-15 20:46:18 -0300

    Adição da api de analise/dashboard

---
+++ user-analysis/src/main/java/com/dbccompany/useranalysis/UserAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.dbccompany.useranalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAnalysisApplication.class, args);
+    }
+
+}
---
+++ user-analysis/src/test/java/com/dbccompany/useranalysis/UserAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbccompany.useranalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
