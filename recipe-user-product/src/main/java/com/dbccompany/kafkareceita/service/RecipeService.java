8581389e88f39c9863b0b2eaafe6c52e2f8c6d8c
flavio <flaviosguian@gmail.com>
2022-04-16 11:34:31 -0300

    fix: recipe e user produzindo logs para o topico

--- recipe-user-product/src/main/java/com/dbccompany/kafkareceita/service/RecipeService.java
+++ recipe-user-product/src/main/java/com/dbccompany/kafkareceita/service/RecipeService.java
@@ -1,9 +1,7 @@
 package com.dbccompany.kafkareceita.service;
 
 
-import com.dbccompany.kafkareceita.dataTransfer.RecipeCreateDTO;
-import com.dbccompany.kafkareceita.dataTransfer.RecipeFormedDTO;
-import com.dbccompany.kafkareceita.dataTransfer.UserFormedDTO;
+import com.dbccompany.kafkareceita.dataTransfer.*;
 import com.dbccompany.kafkareceita.entity.RecipeEntity;
 import com.dbccompany.kafkareceita.entity.UserEntity;
 import com.dbccompany.kafkareceita.exceptions.ObjectNotFoundException;
@@ -25,19 +23,26 @@ public class RecipeService {
     private final RecipeRepository recipeRepository;
     private final ObjectMapper objectMapper;
     private final UserService userService;
+    private final ProducerService producerService;
+    private final static LogDTO logInfo = new LogDTO();
 
-    public List<RecipeFormedDTO> readAllRecipes() {
+
+    public List<RecipeFormedDTO> readAllRecipes() throws JsonProcessingException {
+        producerService.sendMessage(logInfo.constroiLog("Chamada de método service:: Encontrar receitas cadastradas."
+                , TypeLog.INFO));
         return convertList(recipeRepository.findAll());
     }
 
-    public RecipeFormedDTO findRecipeById(String idRecipe) throws ObjectNotFoundException {
-        log.info("Chamada de método service:: Encontrar por id.");
+    public RecipeFormedDTO findRecipeById(String idRecipe) throws ObjectNotFoundException, JsonProcessingException {
+        producerService.sendMessage(logInfo.constroiLog("Chamada de método service:: Encontrar por id."
+                , TypeLog.INFO));
         RecipeEntity r = recipeRepository.findById(idRecipe).orElseThrow(() -> new ObjectNotFoundException("Recipe not found!"));
         return objectMapper.convertValue(r, RecipeFormedDTO.class);
     }
 
     public RecipeFormedDTO saveRecipe(RecipeCreateDTO recipeCreateDTO, String idUser) throws ObjectNotFoundException, UserNotActiveException, JsonProcessingException {
-        log.info("Chamada de método service:: Salvar receitas.");
+        producerService.sendMessage(logInfo.constroiLog("Chamada de método service:: Salvar receitas."
+                , TypeLog.INFO));
         UserFormedDTO userFormedDTO = userService.findUserById(idUser);
         UserEntity userEntity = objectMapper.convertValue(userFormedDTO, UserEntity.class);
         if (!userEntity.isActive()) {
@@ -51,8 +56,9 @@ public class RecipeService {
         return objectMapper.convertValue(r2, RecipeFormedDTO.class);
     }
 
-    public RecipeFormedDTO updateRecipe(RecipeCreateDTO recipeCreateDTO, String idRecipe) throws ObjectNotFoundException, UserNotActiveException {
-        log.info("Chamada de método service:: Atualizar receitas.");
+    public RecipeFormedDTO updateRecipe(RecipeCreateDTO recipeCreateDTO, String idRecipe) throws ObjectNotFoundException, UserNotActiveException, JsonProcessingException {
+        producerService.sendMessage(logInfo.constroiLog("Chamada de método service:: Atualizar receitas."
+                , TypeLog.INFO));
         RecipeEntity oldRecipe = recipeRepository.findById(idRecipe)
                 .orElseThrow(() -> new ObjectNotFoundException("Recipe not found!"));
         if (!oldRecipe.getAuthor().isActive()) {
@@ -72,8 +78,9 @@ public class RecipeService {
         return objectMapper.convertValue(recipeReturn, RecipeFormedDTO.class);
     }
 
-    public void deleteRecipe(String idRecipe) throws ObjectNotFoundException {
-        log.info("Chamada de método service:: Deletar receitas.");
+    public void deleteRecipe(String idRecipe) throws ObjectNotFoundException, JsonProcessingException {
+        producerService.sendMessage(logInfo.constroiLog("Chamada de método service:: Deletar receitas."
+                , TypeLog.INFO));
         RecipeEntity recipeEntity = recipeRepository.findById(idRecipe).orElseThrow(() ->
                 new ObjectNotFoundException("Recipe not found!"));
         recipeRepository.delete(recipeEntity);
